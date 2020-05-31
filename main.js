c437c8ff7a047f230c8442697bb3b4aa4002debf
nazaretyma <naryceita@gmail.com>
2020-05-31 18:08:58 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function ligarLamp () {
+    document.getElementById ("Lampada").src = "img/onbulb.jpg";
+
+}
+
+
+function apagarLamp () {
+    document.getElementById ("Lampada").src = "img/offbulb.jpg";
+    
+}
